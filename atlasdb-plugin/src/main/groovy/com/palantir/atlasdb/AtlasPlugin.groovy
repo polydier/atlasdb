dbb967b52d7bea43ba97d7728fad8cd13983b854
Bobby Ullman <bullman@palantir.com>
2016-05-03 13:00:17 -0700

    add schema list configuration

--- atlasdb-plugin/src/main/groovy/com/palantir/atlasdb/AtlasPlugin.groovy
+++ atlasdb-plugin/src/main/groovy/com/palantir/atlasdb/AtlasPlugin.groovy
@@ -3,11 +3,14 @@ package com.palantir.atlasdb
 import org.gradle.api.Plugin
 import org.gradle.api.Project;
 
-
 class AtlasPlugin implements Plugin<Project> {
-
     @Override
     void apply(Project project) {
+        project.extensions.create("atlasdb", AtlasPluginExtension) 
         project.apply(from: "${project.rootProject.projectDir}/buildSrc/atlasPlugin.gradle")
     }
+}
+
+class AtlasPluginExtension {
+    String[] schemas
 }
