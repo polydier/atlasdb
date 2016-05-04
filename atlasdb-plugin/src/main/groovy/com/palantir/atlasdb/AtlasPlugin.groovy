c185f74bae851f26bfa935ec1cbaef19c9752636
Bobby Ullman <bullman@palantir.com>
2016-05-04 14:41:30 -0700

    only configure plugins when they are enabled

--- atlasdb-plugin/src/main/groovy/com/palantir/atlasdb/AtlasPlugin.groovy
+++ atlasdb-plugin/src/main/groovy/com/palantir/atlasdb/AtlasPlugin.groovy
@@ -6,27 +6,19 @@ import org.gradle.api.plugins.JavaPlugin
 import org.gradle.api.plugins.quality.CheckstylePlugin
 import org.gradle.api.plugins.quality.FindBugsPlugin
 import org.gradle.plugins.ide.eclipse.EclipsePlugin
-import org.gradle.plugins.ide.idea.IdeaPlugin;
+import org.gradle.plugins.ide.idea.IdeaPlugin
 
 class AtlasPlugin implements Plugin<Project> {
     @Override
     void apply(Project project) {
+        project.plugins.apply JavaPlugin
         project.extensions.create("atlasdb", AtlasPluginExtension)
 
         project.tasks.create("generateSchemas", GenerateSchemasTask.class)
         project.tasks.create("cleanSchemas", CleanSchemasTask.class)
 
-        applyPlugins(project)
         setupGeneratedSourceSet(project)
         setupTaskDependencies(project)
-    }
-
-    void applyPlugins(Project project) {
-        project.plugins.apply JavaPlugin
-        project.plugins.apply IdeaPlugin
-        project.plugins.apply EclipsePlugin
-        project.plugins.apply CheckstylePlugin
-        project.plugins.apply FindBugsPlugin
     }
 
     void setupGeneratedSourceSet(Project project) {
@@ -34,10 +26,12 @@ class AtlasPlugin implements Plugin<Project> {
             generated
         }
 
-        project.idea {
-            module {
-                sourceDirs += project.file('src/generated/java')
-                generatedSourceDirs += project.file('src/generated/java')
+        project.plugins.withType(IdeaPlugin) {
+            project.idea {
+                module {
+                    sourceDirs += project.file('src/generated/java')
+                    generatedSourceDirs += project.file('src/generated/java')
+                }
             }
         }
 
@@ -49,12 +43,20 @@ class AtlasPlugin implements Plugin<Project> {
     void setupTaskDependencies(Project project) {
         project.tasks.compileGeneratedJava.dependsOn project.tasks.generateSchemas
         project.tasks.check.dependsOn project.tasks.compileGeneratedJava
-        project.tasks.eclipse.dependsOn project.tasks.generateSchemas
-        project.tasks.idea.dependsOn project.tasks.generateSchemas
         project.tasks.clean.dependsOn project.tasks.cleanSchemas
+        project.plugins.withType(EclipsePlugin) {
+            project.tasks.eclipse.dependsOn project.tasks.generateSchemas
+        }
+        project.plugins.withType(IdeaPlugin) {
+            project.tasks.idea.dependsOn project.tasks.generateSchemas
+        }
 
-        project.tasks.checkstyleGenerated.enabled = false
-        project.tasks.findbugsGenerated.enabled = false
+        project.plugins.withType(CheckstylePlugin) {
+            project.tasks.checkstyleGenerated.enabled = false
+        }
+        project.plugins.withType(FindBugsPlugin) {
+            project.tasks.findbugsGenerated.enabled = false
+        }
 
         project.jar {
             from project.sourceSets.generated.output
--- atlasdb-plugin/src/test/groovy/com/palantir/atlasdb/AtlasPluginTest.groovy
+++ atlasdb-plugin/src/test/groovy/com/palantir/atlasdb/AtlasPluginTest.groovy
@@ -1,6 +1,10 @@
 package com.palantir.atlasdb
 
 import org.gradle.api.Project
+import org.gradle.api.plugins.quality.CheckstylePlugin
+import org.gradle.api.plugins.quality.FindBugsPlugin
+import org.gradle.plugins.ide.eclipse.EclipsePlugin
+import org.gradle.plugins.ide.idea.IdeaPlugin
 import org.gradle.testfixtures.ProjectBuilder
 import org.junit.Before
 import org.junit.Test
@@ -11,6 +15,10 @@ class AtlasPluginTest {
     @Before
     public void setup() {
         project = ProjectBuilder.builder().build()
+        project.plugins.apply IdeaPlugin
+        project.plugins.apply EclipsePlugin
+        project.plugins.apply CheckstylePlugin
+        project.plugins.apply FindBugsPlugin
         project.plugins.apply AtlasPlugin
     }
 
